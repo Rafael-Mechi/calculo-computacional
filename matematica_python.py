af6b2c9bb25f4d9fd36c8a2fe9a37f04fe827a18
Rafael-Mechi <rafael.doliveira@sptech.school>
2025-08-11 22:35:37 -0300

    matematica_python concluido

--- matematica_python.py
+++ matematica_python.py
@@ -22,8 +22,8 @@ print ("a/b=",divisao)
 
 #
 print ('\n'*50)
-a1=6,5
-b1=2,5
+a1=6.5
+b1=2.5
 # Vamos fazer operações mais elaboradas
 # usando as 4 operações simples
 soma = a1+b1
@@ -39,9 +39,6 @@ print ("a*b=",multiplicacao)
 print ("a/b=",divisao)
 
 # Se houver algum erro apontado pelo interpretador nas operações acima corrija
-
-
-import math
 
 # Exercício 1)
 # Atribua o valor 2 a uma variável chamada a.
@@ -70,3 +67,177 @@ print(f"Potência ao quadrado: {potencia_a_quarta}")
 # • Atribua valores para duas variáveis:
 # o c (ex.: 4)
 # o d (ex.: 5)
+
+import math
+c_str = input("Digite o valor de c: ")
+d_str = input("Digite o valor de d: ")
+
+c = int(c_str)
+d = int(d_str)
+
+#• Calcule usando pow() e armazene em variáveis:
+#o c_elevado_ao_quadrado → c²
+#o c_elevado_ao_cubo → c³
+#o c_elevado_a_quarta → c⁴
+#o c_elevado_a_d → c elevado a d
+
+c_elevado_ao_quadrado = pow(c, 2)
+c_elevado_ao_cubo = pow(c, 3)
+c_elevado_a_quarta = pow(c, 4)
+c_elevado_a_d = pow(c, d)
+
+#Exiba os resultados com mensagens no formato:
+
+ #c elevado ao quadrado = <valor>
+ #c elevado ao cubo = <valor>
+ #c elevado a quarta = <valor>
+ #c elevado a d = <valor>
+
+print(f"c elevado ao quadrado = {c_elevado_ao_quadrado}\n")
+print(f"c elevado ao cubo = {c_elevado_ao_cubo}\n")
+print(f"c elevado a quarta = {c_elevado_a_quarta}\n")
+print(f"c elevado a d = {c_elevado_a_d}\n")
+
+"""
+Exercício 3)
+Faça um código em Python
+• Cria uma variável x=512
+• Calcule:
+o raiz_quadrada_de_x =
+o raiz_cúbica_de_x =
+o raiz_quarta_de_x =
+• Imprima o resultado das strings
+"""
+
+x = 512
+raiz_quadrada_x = math.sqrt(x);
+print(raiz_quadrada_x)
+
+raiz_cubida_x = pow(x, 1/3)
+print(raiz_cubida_x)
+
+raiz_quarta_x = pow(x, 1/4)
+print(raiz_quarta_x)
+
+"""
+Exercício 4)
+Dado o valor de w anteriormente, import as funções floor, ceil da biblioteca math e determine o
+piso, o teto e o arredondamento. (usar floor, ceil e round)
+
+"""
+
+w = 3345.61
+
+w_piso = math.floor(w)
+w_teto = math.ceil(w)
+w_arredondado = round(w)
+
+print(f"Salário: {w}")
+print(f"Piso: {w_piso}")
+print(f"Teto: {w_teto}")
+print(f"Arredondado: {w_arredondado}")
+
+"""
+Exercício5)
+• Teste a função round usando casas decimais, inclusive 0 casas
+• Por que não importamos o round?
+"""
+w = 3345.6123849
+w_arredondado = round(w)
+print(w_arredondado)
+
+
+w = 3345
+w_arredondado = round(w)
+print(w_arredondado)
+
+# Não importamos o round porque ela, assim como outras funções (pow, print, len) já vem programadas em bibliotecas ao instalar o python
+
+"""
+Exercício 6)
+Arredonde os números a seguir, exibindo sem casas decimais:
+x1=1.456
+x2=3.678
+x3=7.5
+"""
+
+x1 = 1.456
+x2 = 3.678
+x3 = 7.5
+
+print(round(x1))
+print(round(x2))
+print(round(x3))
+
+"""
+Exercício 7)
+Voltando ao caso do floor e ceil, por padrão estas funções retornam valores inteiros. Podemos
+determinar que os valores resultantes possuam ponto flutuante (float):
+"""
+
+# Faça o teste a seguir:
+print('\n'*2)
+resultado = math.floor(1.456)
+print(resultado)
+print(type(resultado))
+resultado_float = float(math.floor(1.456))
+print(resultado_float)
+print(type(resultado_float))
+
+# Creio que o enunciado pede para dizermos qual variável retorna um tipo float. Portanto, é a variável "resultado_float",
+# pois a função "float()" garante que o número tenha casas decimais, enquanto floor arredonda para o piso
+
+"""
+Super Exercício 8)
+Resolva em Python as expressões a seguir:
+"""
+
+q1 = pow(2, 3)
+print(q1)
+
+q2 = -(pow(2, 3))
+print(q2)
+
+q3 = pow(1, 0)
+print(q3)
+
+q4 = -(pow(1, 0))
+print(q4)
+
+q5 = pow(2, 0)
+print(q5)
+
+q6 = pow(2/5, 3)
+print(q6)
+
+q7 = pow(3, -2)
+print(q7)
+
+q8 = pow (1/2, -3)
+print(q8)
+
+q9 = pow(-1, 3) ** 4
+print(q9)
+
+q10 = pow(0.5, 3)
+print(q10)
+
+q11 = pow(0.25, 4)
+print(q11)
+
+q12 = pow(0, 4)
+print(q12)
+
+q13 = 1 + pow(0.41, 2)
+print(q13)
+
+q14 = 1/4 + pow(5, 2) - pow(-2, -4)
+print(q14)
+
+q15 = pow(2, -3) + pow(-4, -5)
+
+q16v1 = (((4/5)-(1/2)+1)**-2) + (1 / (1 + 3**2 - (4-5)**-2))
+print(q16v1)
+
+q16v2 = pow((4/5-1/2)+1, -2) + (1 / (1 + pow(3, 2) - (pow(4-5, -2))))
+print(q16v2)
